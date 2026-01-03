52f69fdf11a5a8453a1d260a2e043a0dd2714edb
Adrien LECOMTE <97622219+ZdarkBlackShadow@users.noreply.github.com>
2026-01-03 18:54:53 +0100

    Create database.go

---
+++ config/database.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"rootme-bot/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func initDB() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
